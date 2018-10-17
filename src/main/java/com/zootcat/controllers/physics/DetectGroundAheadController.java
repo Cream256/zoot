af590f7b6b3c97fa2820febecbcc5747cb4c325d
Cream256 <cream256@wp.pl>
2018-10-17 06:45:28 +0200

    DetectGroundAhead ctrl remake

--- src/main/java/com/zootcat/controllers/physics/DetectGroundAheadController.java
+++ src/main/java/com/zootcat/controllers/physics/DetectGroundAheadController.java
@@ -1,48 +1,93 @@
 package com.zootcat.controllers.physics;
 
 import com.badlogic.gdx.math.collision.BoundingBox;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.zootcat.controllers.factory.CtrlDebug;
+import com.zootcat.controllers.factory.CtrlParam;
 import com.zootcat.controllers.logic.DirectionController;
+import com.zootcat.fsm.events.ZootEvent;
 import com.zootcat.fsm.events.ZootEventType;
 import com.zootcat.fsm.events.ZootEvents;
 import com.zootcat.math.ZootBoundingBoxFactory;
 import com.zootcat.scene.ZootActor;
 import com.zootcat.scene.ZootDirection;
 
-public class DetectGroundAheadController extends DetectGroundController
+/**
+ * DetectGround controller - Creates a sensor, that is detecting if
+ * the actor has ground in front of him. If not, controller emits NoGroundAhead {@link ZootEvent}.
+ * 
+ * @ctrlParam useActorSize - If set, actor width and 10% of actor height will be used
+ * to set sensor size. Otherwise sensorWidth and sensorHeight will be used
+ * 
+ * @author Cream
+ */
+public class DetectGroundAheadController extends OnCollideWithSensorController
 {
-	private BoundingBox boundingBox;
-	private ZootDirection direction = ZootDirection.None;
+	public static final float SENSOR_HEIGHT_PERCENT = 0.2f;
 	
-	@Override
-	public void init(ZootActor actor)
-	{
-		super.init(actor);
-		boundingBox = new BoundingBox();
-	}
+	@CtrlParam private boolean useActorSize = true;
+	@CtrlDebug private boolean isGroundAhead = false;
+	@CtrlDebug private ZootDirection direction = ZootDirection.None;
+	private BoundingBox boundingBox = new BoundingBox();
 	
 	@Override
 	public void onAdd(ZootActor actor)
 	{
+		setSensorSize(actor);
+		super.setCollideWithSensors(true);
 		super.onAdd(actor);
 		setSensorPositionToFaceActorDirection(actor);
 	}
 	
+	private void setSensorSize(ZootActor actor)
+	{		
+		super.sensorWidth = calculateSensorWidth(actor);
+		super.sensorHeight = calculateSensorHeight(actor);								
+	}
+	
+	private float calculateSensorWidth(ZootActor actor)
+	{
+		return useActorSize ? actor.getWidth() / super.getScene().getUnitScale() : sensorWidth;
+	}
+	
+	private float calculateSensorHeight(ZootActor actor)
+	{
+		return useActorSize ? (actor.getHeight() / super.getScene().getUnitScale()) * SENSOR_HEIGHT_PERCENT : sensorHeight;		
+	}
+
 	@Override
 	public void preUpdate(float delta, ZootActor actor)
 	{
-		super.preUpdate(delta, actor);
+		isGroundAhead = false;
 		setSensorPositionToFaceActorDirection(actor);
 	}
-	
+
 	@Override
 	public void postUpdate(float delta, ZootActor actor)
 	{
-		if(!isOnGround())
+		if(!isGroundAhead) 
 		{
 			ZootEvents.fireAndFree(actor, ZootEventType.NoGroundAhead);
-		}	
+		}
+	}
+	
+	@Override
+	protected SensorCollisionResult onCollideWithSensor(Fixture fixture)
+	{
+		if(fixture.isSensor())
+		{
+			return SensorCollisionResult.ProcessNext;
+		}
+		
+		isGroundAhead = true;
+		return SensorCollisionResult.StopProcessing;
 	}
 
+	public boolean isGroundAhead()
+	{
+		return isGroundAhead;
+	}	
+	
 	private void setSensorPositionToFaceActorDirection(ZootActor actor)
 	{
 		ZootDirection oldDirection = direction;		
--- src/test/java/com/zootcat/controllers/physics/DetectGroundAheadControllerTest.java
+++ src/test/java/com/zootcat/controllers/physics/DetectGroundAheadControllerTest.java
@@ -1,6 +1,6 @@
 package com.zootcat.controllers.physics;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.zootcat.controllers.factory.ControllerAnnotations;
@@ -26,6 +27,8 @@ import com.zootcat.scene.ZootScene;
 
 public class DetectGroundAheadControllerTest
 {
+	private static final float ACTOR_WIDTH = 20.0f;
+	private static final float ACTOR_HEIGHT = 10.0f;
 	private static final float SENSOR_WIDTH = 10.0f;
 	private static final float SENSOR_HEIGHT = 5.0f;
 	
@@ -56,6 +59,7 @@ public class DetectGroundAheadControllerTest
 		when(physicsCtrl.addFixture(any(), any())).thenReturn(fixture);
 		when(fixture.getShape()).thenReturn(shape);
 		when(fixture.getBody()).thenReturn(mock(Body.class));
+		when(directionCtrl.getDirection()).thenReturn(ZootDirection.None);
 		
 		eventCounter = new ZootActorEventCounterListener();
 		
@@ -120,5 +124,82 @@ public class DetectGroundAheadControllerTest
 		//then
 		assertEquals(1, eventCounter.getCount());
 		assertEquals(ZootEventType.NoGroundAhead, eventCounter.getLastZootEvent().getType());
+	}
+	
+	@Test
+	public void shouldUseActorSizeWhenCreatingSensor()
+	{
+		//given
+		ControllerAnnotations.setControllerParameter(ctrl, "useActorSize", true);
+		
+		//when
+		ctrlActor.setSize(ACTOR_WIDTH, ACTOR_HEIGHT);		
+		ctrl.init(ctrlActor);
+		ctrl.onAdd(ctrlActor);
+		
+		//then
+		assertEquals("Should use actor width", ACTOR_WIDTH, ctrl.sensorWidth, 0.0f);
+		assertEquals("Should use actor height", ACTOR_HEIGHT * DetectGroundAheadController.SENSOR_HEIGHT_PERCENT, ctrl.sensorHeight, 0.0f);
+	}
+	
+	@Test
+	public void shouldUseProvidedSizeWhenCreatingSensor()
+	{
+		//given
+		ControllerAnnotations.setControllerParameter(ctrl, "useActorSize", false);
+		ControllerAnnotations.setControllerParameter(ctrl, "sensorWidth", SENSOR_WIDTH);
+		ControllerAnnotations.setControllerParameter(ctrl, "sensorHeight", SENSOR_HEIGHT);
+				
+		//when		
+		ctrl.init(ctrlActor);
+		ctrl.onAdd(ctrlActor);
+		
+		//then
+		assertEquals("Should use provided width", SENSOR_WIDTH, ctrl.sensorWidth, 0.0f);
+		assertEquals("Should use provided height", SENSOR_HEIGHT, ctrl.sensorHeight, 0.0f);		
+	}
+	
+	@Test
+	public void shouldNotDetectSensorAsGround()
+	{
+		//given
+		Contact contact = mock(Contact.class);
+		ZootActor otherActor = mock(ZootActor.class);
+		Fixture otherActorFixture = mock(Fixture.class);
+				
+		//when
+		when(contact.getFixtureA()).thenReturn(fixture);
+		when(contact.getFixtureB()).thenReturn(otherActorFixture);
+		when(otherActorFixture.isSensor()).thenReturn(true);
+		
+		ctrl.init(ctrlActor);
+		ctrl.onAdd(ctrlActor);
+		ctrl.onEnter(ctrlActor, otherActor, contact);		
+		ctrl.onUpdate(1.0f, ctrlActor);
+		
+		//then
+		assertFalse(ctrl.isGroundAhead());
+	}
+	
+	@Test
+	public void shouldDetectFixtureAsGround()
+	{
+		//given
+		Contact contact = mock(Contact.class);
+		ZootActor otherActor = mock(ZootActor.class);
+		Fixture otherActorFixture = mock(Fixture.class);
+				
+		//when
+		when(contact.getFixtureA()).thenReturn(fixture);
+		when(contact.getFixtureB()).thenReturn(otherActorFixture);
+		when(otherActorFixture.isSensor()).thenReturn(false);
+		
+		ctrl.init(ctrlActor);
+		ctrl.onAdd(ctrlActor);
+		ctrl.onEnter(ctrlActor, otherActor, contact);		
+		ctrl.onUpdate(1.0f, ctrlActor);
+		
+		//then
+		assertTrue(ctrl.isGroundAhead());
 	}
 }
