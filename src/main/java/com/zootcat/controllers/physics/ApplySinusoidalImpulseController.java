8b3b279a8ea2851dbbd99cded8c34b6048d3c7dc
Cream256 <cream256@wp.pl>
2018-10-29 07:19:35 +0100

    sinusoidal impulse ctrl

---
+++ src/main/java/com/zootcat/controllers/physics/ApplySinusoidalImpulseController.java
@@ -0,0 +1,42 @@
+package com.zootcat.controllers.physics;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.zootcat.controllers.ControllerAdapter;
+import com.zootcat.controllers.factory.CtrlParam;
+import com.zootcat.scene.ZootActor;
+
+public class ApplySinusoidalImpulseController extends ControllerAdapter
+{
+	@CtrlParam(debug = true) private float impulseX = 1.0f;
+	@CtrlParam(debug = true) private float impulseY = 1.0f;
+	@CtrlParam(debug = true) private float mul = 1.0f;
+
+	private float time = 0.0f;
+		
+	@Override
+	public void onUpdate(float delta, ZootActor actor)
+	{
+		time += delta;	
+		
+		float sin = MathUtils.sin(time * mul);	
+		float ix = impulseX * sin;
+		float iy = impulseY * sin;
+		
+		actor.controllerAction(PhysicsBodyController.class, ctrl -> ctrl.applyImpulse(ix, iy));
+	}
+	
+	public void setImpulseX(float value)
+	{
+		impulseX = value;
+	}
+	
+	public void setImpulseY(float value)
+	{
+		impulseY = value;
+	}
+	
+	public void setMul(float value)
+	{
+		mul = value;
+	}
+}
---
+++ src/test/java/com/zootcat/controllers/physics/ApplySinusoidalImpulseControllerTest.java
@@ -0,0 +1,53 @@
+package com.zootcat.controllers.physics;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.zootcat.scene.ZootActor;
+
+public class ApplySinusoidalImpulseControllerTest
+{
+	private ZootActor actor;
+	private ApplySinusoidalImpulseController ctrl;
+	@Mock private PhysicsBodyController physicsBodyCtrl;
+			
+	@Before
+	public void setup()
+	{
+		MockitoAnnotations.initMocks(this);
+		
+		actor = new ZootActor();
+		actor.addController(physicsBodyCtrl);
+		
+		ctrl = new ApplySinusoidalImpulseController();
+	}
+
+	@Test
+	public void shouldApplyDefaultValues()
+	{
+		ctrl.onUpdate(MathUtils.PI / 2.0f, actor);
+		verify(physicsBodyCtrl).applyImpulse(1.0f, 1.0f);	
+	}
+	
+	@Test
+	public void shouldSetTimeMultiplier()
+	{
+		ctrl.setMul(2.0f);
+		ctrl.onUpdate(MathUtils.PI, actor);
+		verify(physicsBodyCtrl).applyImpulse(0.0f, 0.0f);
+	}
+	
+	@Test
+	public void shouldSetImpulses()
+	{
+		ctrl.setImpulseX(2.0f);
+		ctrl.setImpulseY(-2.0f);
+		ctrl.onUpdate(MathUtils.PI / 2.0f, actor);
+		verify(physicsBodyCtrl).applyImpulse(2.0f, -2.0f);
+	}
+}
