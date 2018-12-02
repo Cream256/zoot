cae1ea9ce32d15c8be9f76a58b61c450d47fb511
Cream256 <cream256@wp.pl>
2018-12-02 16:11:01 +0100

    SetApplyImpulseDirectionOnCollide controller

---
+++ src/main/java/com/zootcat/controllers/physics/SetApplyImpulseDirectionOnCollideController.java
@@ -0,0 +1,37 @@
+package com.zootcat.controllers.physics;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.zootcat.controllers.factory.CtrlParam;
+import com.zootcat.scene.ZootActor;
+import com.zootcat.scene.ZootDirection;
+
+public class SetApplyImpulseDirectionOnCollideController extends OnCollideController
+{
+	@CtrlParam private boolean setX = true;
+	@CtrlParam private boolean setY = true;
+	@CtrlParam private ZootDirection directionX = ZootDirection.None;
+	@CtrlParam private ZootDirection directionY = ZootDirection.None;
+	
+	@Override
+	public void onEnter(ZootActor actorA, ZootActor actorB, Contact contact)
+	{
+		getOtherActor(actorA, actorB).controllerAction(ApplyImpulseController.class, ctrl -> 
+		{
+			if(setX && Math.signum(ctrl.getImpulseX()) != Math.signum(directionX.getHorizontalValue())) 
+			{
+				ctrl.setImpulseX(ctrl.getImpulseX() * -1);
+			}
+			
+			if(setY && Math.signum(ctrl.getImpulseY()) != Math.signum(directionY.getVerticalValue())) 
+			{
+				ctrl.setImpulseY(ctrl.getImpulseY() * -1);
+			}
+		});
+	}
+
+	@Override
+	public void onLeave(ZootActor actorA, ZootActor actorB, Contact contact)
+	{
+		//noop
+	}
+}
---
+++ src/test/java/com/zootcat/controllers/physics/SetApplyImpulseDirectionOnCollideControllerTest.java
@@ -0,0 +1,153 @@
+package com.zootcat.controllers.physics;
+
+import static org.mockito.Matchers.anyFloat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.zootcat.controllers.factory.ControllerAnnotations;
+import com.zootcat.scene.ZootActor;
+import com.zootcat.scene.ZootDirection;
+
+public class SetApplyImpulseDirectionOnCollideControllerTest
+{
+	@Mock private ZootActor ctrlActor;
+	@Mock private ApplyImpulseController applyImpulseCtrl;
+
+	private ZootActor otherActor;
+	private SetApplyImpulseDirectionOnCollideController ctrl;
+	
+	@Before
+	public void setup()
+	{
+		MockitoAnnotations.initMocks(this);
+		
+		otherActor = new ZootActor();
+		otherActor.addController(applyImpulseCtrl);
+		
+		ctrl = new SetApplyImpulseDirectionOnCollideController();
+		ctrl.init(ctrlActor);
+	}
+	
+	@Test
+	public void shouldSetNewHorizontalDirection()
+	{
+		//given
+		final float impulseX = -2.0f;
+		when(applyImpulseCtrl.getImpulseX()).thenReturn(impulseX);
+		
+		ControllerAnnotations.setControllerParameter(ctrl, "setX", true);
+		ControllerAnnotations.setControllerParameter(ctrl, "directionX", ZootDirection.Right);		
+		
+		//when		
+		ctrl.onEnter(ctrlActor, otherActor, mock(Contact.class));
+		
+		//then
+		verify(applyImpulseCtrl).setImpulseX(-impulseX);
+	}
+	
+	@Test
+	public void shouldNotSetNewHorizontalDirectionIfDirectionIsTheSame()
+	{
+		//given
+		final float impulseX = 2.0f;
+		when(applyImpulseCtrl.getImpulseX()).thenReturn(impulseX);
+		
+		ControllerAnnotations.setControllerParameter(ctrl, "setX", true);
+		ControllerAnnotations.setControllerParameter(ctrl, "directionX", ZootDirection.Right);		
+		
+		//when		
+		ctrl.onEnter(ctrlActor, otherActor, mock(Contact.class));
+		
+		//then
+		verify(applyImpulseCtrl, never()).setImpulseX(anyFloat());				
+	}
+	
+	@Test
+	public void shouldNotSetNewHorizontalDirectionIfNotAllowed()
+	{
+		//given
+		final float impulseX = -2.0f;
+		when(applyImpulseCtrl.getImpulseX()).thenReturn(impulseX);
+		
+		ControllerAnnotations.setControllerParameter(ctrl, "setX", false);
+		ControllerAnnotations.setControllerParameter(ctrl, "directionX", ZootDirection.Right);		
+		
+		//when		
+		ctrl.onEnter(ctrlActor, otherActor, mock(Contact.class));
+		
+		//then
+		verify(applyImpulseCtrl, never()).setImpulseX(anyFloat());
+	}
+	
+	@Test
+	public void shouldSetNewVerticalDirection()
+	{
+		//given
+		final float impulseY = -2.0f;
+		when(applyImpulseCtrl.getImpulseY()).thenReturn(impulseY);
+		
+		ControllerAnnotations.setControllerParameter(ctrl, "setY", true);
+		ControllerAnnotations.setControllerParameter(ctrl, "directionY", ZootDirection.Up);		
+		
+		//when		
+		ctrl.onEnter(ctrlActor, otherActor, mock(Contact.class));
+		
+		//then
+		verify(applyImpulseCtrl).setImpulseY(-impulseY);
+	}
+	
+	@Test
+	public void shouldNotSetNewVerticalDirectionIfDirectionIsTheSame()
+	{
+		//given
+		final float impulseY = 2.0f;
+		when(applyImpulseCtrl.getImpulseY()).thenReturn(impulseY);
+		
+		ControllerAnnotations.setControllerParameter(ctrl, "setY", true);
+		ControllerAnnotations.setControllerParameter(ctrl, "directionY", ZootDirection.Up);		
+		
+		//when		
+		ctrl.onEnter(ctrlActor, otherActor, mock(Contact.class));
+		
+		//then
+		verify(applyImpulseCtrl, never()).setImpulseY(anyFloat());			
+	}
+	
+	@Test
+	public void shouldNotSetNewVerticalDirectionIfNotAllowed()
+	{
+		//given
+		final float impulseY = -2.0f;
+		when(applyImpulseCtrl.getImpulseY()).thenReturn(impulseY);
+		
+		ControllerAnnotations.setControllerParameter(ctrl, "setY", false);
+		ControllerAnnotations.setControllerParameter(ctrl, "directionY", ZootDirection.Up);		
+		
+		//when		
+		ctrl.onEnter(ctrlActor, otherActor, mock(Contact.class));
+		
+		//then
+		verify(applyImpulseCtrl, never()).setImpulseY(anyFloat());	
+	}
+	
+	@Test
+	public void shouldDoNothingOnLeave()
+	{
+		Contact contact = mock(Contact.class);
+		ZootActor actorA = mock(ZootActor.class);
+		ZootActor actorB = mock(ZootActor.class);
+		
+		ctrl.onLeave(actorA, actorB, contact);
+		verifyNoMoreInteractions(actorA, actorB, contact);
+	}
+	
+}
